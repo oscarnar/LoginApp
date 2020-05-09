84b3df853ef2e0293db5d3a57ee6d6fe61d424a7
oscarnar <damon@damon.localdomain>
2020-05-08 23:49:27 -0500

    Agregue imagenes para cada curso

--- lib/providers/courseProvider.dart
+++ lib/providers/courseProvider.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/widgets.dart';
 import 'package:login_app/models/course.dart';
 
-//Solo para probar
-String urlTemp = 'https://elbocon.pe/resizer/cHDiKi4eFoBXM_RBFVlCkD9AIQI=/1200x800/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/PDGLLUK4ZJCL3GNZSJZFL3BVGQ.jpg';
-
 class CourseProvider with ChangeNotifier {
-  //List<Course> listCourses = [];
   List<Course> listCourses = [
-    Course("Computacion Grafica", description: "Jugamos con imagenes", urlPhoto: urlTemp),
-    Course("Inteligencia Arificial", description: "Algoritmos basicos de IA", urlPhoto: urlTemp),
-    Course("Redes y Comunicacion", description: "Redes", urlPhoto: urlTemp),
-    Course("Investigacion en CS", description: "Leer papers y buscar y buscar temas para investigar", urlPhoto: urlTemp),
-    Course("Ingenieria de Software", description: "Conceptos basicos para desarrollar", urlPhoto: urlTemp),
+    Course(
+      "Computacion Grafica",
+      description: "Jugamos con imagenes",
+      urlPhoto:
+          'https://2.bp.blogspot.com/-BrRiO6nA_8s/WgiC1fU_ZOI/AAAAAAAACPM/TcKaWnR9lgwEDCcu7v3nb69pLlLVdYdgwCLcBGAs/s1600/futurocomg.jpg',
+    ),
+    Course("Inteligencia Arificial",
+        description: "Algoritmos basicos de IA",
+        urlPhoto:
+            'https://observatorio-ia.com/wp-content/uploads/2019/04/IA-tendencias.jpg'),
+    Course(
+      "Redes y Comunicacion",
+      description: "Redes",
+      urlPhoto:
+          'https://lh3.googleusercontent.com/proxy/2tA9kzmgX_PUh99dlsu72XY1sa2Z53iHNpbPWLd_VJlGFNk9A04iQlffleFg7vlMrrSIEQNXBzHBRxGgR3v9cLEwJJ0jPUC7yXJ9l2ZAS5OIxX9vrAglBATb5wwQCtdhCsYXk8iw4e4W',
+    ),
+    Course(
+      "Investigacion en CS",
+      description: "Leer papers y buscar temas para investigar",
+      urlPhoto:
+          'https://image.shutterstock.com/image-photo/man-working-laptop-connecting-networking-260nw-403797997.jpg',
+    ),
+    Course(
+      "Ingenieria de Software",
+      description: "Conceptos basicos para desarrollar",
+      urlPhoto:
+          'https://www.queestudiar.org/wp-content/uploads/2017/10/software-750x350.jpg',
+    ),
   ];
 
   void addCourse(Course newCourse) {
